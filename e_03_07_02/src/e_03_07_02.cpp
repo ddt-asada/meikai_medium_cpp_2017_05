d2174c5e5b882970c993376c91b9e9d2397faf3d
ddt-asada <ddt.asada@gmail.com>
2017-05-25 19:53:43 +0900

    C++ 演習3-7 100％ 初回コミット （浅田 添削はまだ。 2次元配列の文字列のソートを行うものとポインタの配列による文字列のソートを行うものの2種類を作成。

---
+++ e_03_07_02/src/e_03_07_02.cpp
@@ -0,0 +1,44 @@
+/*演習3-7-2
+ * qsort関数を用いて、配列を昇順にソートするプログラム。
+ * 作成日：2017年5月25日
+ * 作成者：浅田　知嗣
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//文字列の比較関数の宣言。
+int char_cmp(const char* a, const char* b);
+
+int main()
+{
+	//ソートするポインタによる文字列の宣言。
+	char arr[][7] = {"LISP", "C", "Ada", "Pascal"};
+
+	//ソートする前の文字列の表示。
+	cout	<<"以下の文字列を昇順にソートします。\n";
+
+	for(int i = 0; i < 4; i++) {
+		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
+	}
+
+	qsort(arr[0], sizeof(arr)/sizeof(arr[0]),sizeof(arr[0]),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
+
+	//ソートし終わった文字列を表示。
+	cout	<<"\n文字列の並び替えが完了しました。\n結果は以下の通りです。\n";
+
+	for(int i = 0; i < 4; i++) {
+		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
+	}
+
+	return 0;
+}
+
+//文字列の比較関数の定義。
+int char_cmp(const char* a, const char* b)
+{
+	return strcmp(a, b);
+}
