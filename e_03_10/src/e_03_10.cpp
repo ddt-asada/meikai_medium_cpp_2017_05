40fdab04d713ee692eae7de4e39ee39794549ac2
ddt-asada <ddt.asada@gmail.com>
2017-05-25 20:39:05 +0900

    C++ 演習3-10 100％ 初回コミット （浅田 添削まだ。

---
+++ e_03_10/src/e_03_10.cpp
@@ -0,0 +1,79 @@
+/*演習3-10
+ * binsearchx関数を用いて文字列の配列からの探索を行うプログラム。
+ * 作成日：2017年5月25日
+ * 作成者：浅田　知嗣
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//線形探索を行う関数の宣言。
+void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*));
+
+//文字列の比較関数の宣言。
+int char_cmp(const char* a, const char*b);
+
+int main()
+{
+	const char *arr[] = {"abc", "def", "ghi", "jkl", "lmn", "opq", "stu", "xyz"};
+
+	char **result;
+
+	const char *key = "stasd";
+
+	result = reinterpret_cast<char**>(binsearchx(&key, arr, sizeof(arr)/sizeof(arr[0]), sizeof(char *), reinterpret_cast<int (*)(const void*, const void*)>(char_cmp)));
+
+	if(result) {
+		cout	<<*result	<<"が一致します。\n";
+	} else {
+		cout	<<"見つかりませんでした。\n";
+	}
+
+	return 0;
+}
+
+//線形探索を行う関数の定義。
+void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*))
+{
+	if(nmemb > 0){
+		const char* x =reinterpret_cast<const char*>(base);
+		size_t pl = 0;			//探索範囲先頭の添字。
+		size_t pr = nmemb - 1;	//探索範囲末尾の添字。
+		size_t pc;				//探索範囲中央の添字。
+
+		while (true) {
+			int comp = compar(key, reinterpret_cast<const void*>(&x[(pc = (pl + pr) / 2) * size]));
+
+			//探索が成功した時。
+			if(comp == 0) {
+				for( ; pc > pl; pc--) {
+					if(compar(key, reinterpret_cast<const void*>(&x[(pc = (pl + pr) / 2) * size]))) {
+						break;
+					}
+				}
+
+				return const_cast<void*>(reinterpret_cast<const void*>(&x[pc * size]));
+			//探索範囲がなくなったとき。
+			} else if (pl == pr) {
+				break;
+			//探索範囲を後半に絞り込む。
+			} else if (comp > 0) {
+				pl = pc + 1;
+			//探索範囲を前半に絞り込む。
+			} else {
+				pr = pc - 1;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+//文字列の比較関数の定義。
+int char_cmp(const char* a, const char* b)
+{
+	return strcmp(a, b);
+}
