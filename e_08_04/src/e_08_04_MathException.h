80c29f17d122040f9088eb3f7d95f3145c04de50
ddt-asada <ddt.asada@gmail.com>
2017-05-31 19:00:48 +0900

    C++ 演習8-4 100％ 初回コミット （浅田 添削まだです。

---
+++ e_08_04/src/e_08_04_MathException.h
@@ -0,0 +1,68 @@
+/*演習8-4
+ * 例外クラスに仮想関数を追加したものの動作を確認するプログラム。
+ * 作成日：2017年5月31日
+ * 作成者：浅田　知嗣
+ */
+
+#ifndef ___Math_Exception
+#define ___Math_Exception
+
+//数値演算による例外。
+class MathException
+{
+public:
+	virtual void display() const
+	{
+		std::cout	<<"数値演算例外\n";
+	}
+};
+
+//0による徐算の例外。
+class DividedByZero : public MathException
+{
+public:
+	void display() const
+	{
+		std::cout	<<"0による除算\n";
+	}
+};
+
+//オーバーフロー
+class Overflow : public MathException
+{
+	int v;
+
+public:
+	Overflow(int val) : v(val) {}
+
+	int value() const
+	{
+		return v;
+	}
+
+	void display() const
+	{
+		std::cout	<<"オーバーフロー（値は"	<<v	<<"）\n";
+	}
+};
+
+//アンダーフロー
+class Underflow : public MathException
+{
+	int v;
+
+public:
+	Underflow(int val) : v(val) {}
+
+	int value() const
+	{
+		return v;
+	}
+
+	void display() const
+	{
+		std::cout	<<"アンダーフロー（値は"	<<v	<<"）\n";
+	}
+};
+
+#endif
