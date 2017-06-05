80b1c0bef347659a7148df7ab7d20bbd69266f4c
ddt-asada <ddt.asada@gmail.com>
2017-06-05 10:46:12 +0900

    C++演習9-9 100％ 初回コミット （浅田 添削まだです。

---
+++ e_09_09/src/e_09_09.cpp
@@ -0,0 +1,86 @@
+/*演習9-9
+ * スタッククラステンプレートにメンバ関数を追加。
+ * 作成日：2017年6月5日
+ * 作成者：浅田　知嗣
+ */
+
+#include <string>
+#include <iostream>
+#include "e_09_09_SmpleStack.h"
+
+using namespace std;
+
+int main()
+{
+	string x;		//スタックに渡す文字列。
+	int xlen;		//スタックに渡す文字列xの長さ。
+
+	//スタックに渡す文字列xの入力を促す。
+	cout	<<"文字列：";
+	cin		>>x;
+
+	xlen = x.length();		//スタックに渡す文字列xの長さを取得。
+
+	//スタックの空き箱を生成するクラスに文字列xの長さを渡す。
+	SimpleStack<char> s(xlen);
+
+	//スタックの容量を確認する。
+	cout	<<"スタックの容量は"	<<s.capacity()	<<"です。\n";
+
+	//スタックが空かどうかを確認する。
+	if(s.is_empty()) {
+		cout	<<"スタックは空です。\n";
+
+	} else {
+		cout	<<"スタックに何かが入ってます。\n";
+	}
+
+	//スタックに関する例外処理の宣言。
+	try {
+		//スタックに文字列xをプッシュする。
+		for(int i = 0; i < xlen; i++) {
+			s.push(x[i]);
+
+			//スタックが満杯かどうかを確認する。
+			if(s.is_full()) {
+				cout	<<"スタックは満杯です。\n";
+
+			} else {
+				cout	<<"スタックは満杯ではありません。\n";
+			}
+		}
+
+		//スタックが空かどうかを確認する。
+		if(s.is_empty()) {
+			cout	<<"スタックは空です。\n";
+
+		} else {
+			cout	<<"スタックに何かが入ってます。\n";
+		}
+
+		//スタックから文字列xをポップして表示し、中身を確認する。
+		for(int i = 0; i < xlen; i++) {
+			//頂上のデータを変更することなく取得しつつ表示。
+			char c = s.peek();
+			cout	<<c;
+
+			//ポップしながら、減らしながら取得して表示。
+			c = s.pop();
+			cout	<<c;
+		}
+
+		cout	<<'\n';
+	}
+
+	//満杯のスタックからプッシュしようとしたときの例外処理。
+	catch(const SimpleStack<char>::Overflow&) {
+		cout	<<"満杯の<char>スタックにプッシュしようとしました。\n";
+	}
+
+	//空のスタックからポップしようとしたときの例外処理。
+	catch(const SimpleStack<char>::Empty&) {
+		cout	<<"空の<char>スタックからポップしようとしました。\n";
+	}
+
+	return 0;
+}
---
+++ e_09_09/src/e_09_09_SmpleStackImplementation.h
@@ -0,0 +1,82 @@
+/*演習9-9
+ * スタッククラステンプレートにメンバ関数を追加。
+ * 作成日：2017年6月5日
+ * 作成者：浅田　知嗣
+ */
+
+#ifndef ___Class_SimpleStackImplementation
+#define ___Class_SimpleStackImplementation
+
+//スタッククラステンプレートのインクルードモデル
+//明示的コンストラクタ
+template<class Type>
+SimpleStack<Type>::SimpleStack(int sz) : size(sz), ptr(0)
+{
+	//スタックの容量を確保する。
+	stk = new Type[size + 1];
+}
+
+//デストラクタ
+template<class Type>
+SimpleStack<Type>::~SimpleStack()
+{
+	//スタックを削除する。
+	delete[] stk;
+}
+
+//プッシュ
+template<class Type>
+Type& SimpleStack<Type>::push(const Type& x)
+{
+	//スタックの容量を超えてプッシュしようとしたときに例外を送出する。
+	if(ptr >= size) {
+		throw Overflow();
+	}
+
+	//スタックにプッシュされたものを格納する
+	return stk[ptr++] = x;
+}
+
+//ポップ
+template<class Type>
+Type SimpleStack<Type>::pop()
+{
+	//空のスタックからポップしようとしたときに例外を送出する。
+	if(ptr <= 0) {
+		throw Empty();
+	}
+
+	//スタックの中身を減らす。
+	return stk[--ptr];
+}
+
+//頂上のデータを変更することなくそのまま返す。
+template<class Type>
+Type& SimpleStack<Type>::peek() const
+{
+	return stk[ptr];
+}
+
+//スタックの容量のゲッタ。
+template<class Type>
+int SimpleStack<Type>::capacity() const
+{
+	return size;
+}
+
+//スタックが満杯かどうかを返す。
+template<class Type>
+bool SimpleStack<Type>::is_full() const
+{
+	return (size <= ptr ? 1 : 0);
+}
+
+//スタックが空であるかどうかを返す。
+template<class Type>
+bool SimpleStack<Type>::is_empty() const
+{
+	return (ptr == 0 ? 1 : 0);
+}
+
+
+#endif
