621cef10f266b9aecc7464620ddf3088c9c67ebf
ddt-asada <ddt.asada@gmail.com>
2017-06-09 12:56:24 +0900

    C++ 演習3-6 修正100％ （浅田 完了。

--- e_03_06_2/src/e_03_06_2.cpp
+++ e_03_06_2/src/e_03_06_2.cpp
@@ -2,6 +2,8 @@
  * bsearch関数を用いて文字列の配列からの探索を行うプログラム。
  * 作成日：2017年5月25日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 //文字列の2次元配列を使用して走査を行う。
@@ -17,11 +19,16 @@ int char_cmp(const char* a, const char*b);
 
 int main()
 {
-	char arr[][5] = {"abc", "def", "ghi", "jkl", "lmn"};
+	char arr[][10] = {"abc", "cefghi", "jkl", "lmn", "lmn", "lmn", "opq", "stu", "xyz", "adw", "afw", "afw", "awfa", "hrg","bfsr", "bmnn", "gesb", "gs", "knzbzd"};
 
-	char *result;
+	char* result;
 
-	char key[] = "dwf";
+	const char key[] = "abc";
+
+	int n = sizeof(arr)/sizeof(arr[0]);
+
+	//まず初めにqsort関数でソートする。
+	qsort(arr[0], n,sizeof(arr[0]),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
 
 	/*第1引数に走査する文字列の配列の先頭要素のアドレスを渡す。
 	 * 第2引数に操作する対象の文字列の2次元配列の行の先頭要素のアドレスを渡す。
@@ -29,10 +36,11 @@ int main()
 	 * 第4引数char型のバイト数を渡す。
 	 * 第5引数に文字列の比較関数を渡す。
 	 */
-	result = reinterpret_cast<char*>(bsearch(&key[0], &arr[0], sizeof(arr)/sizeof(arr[0]), sizeof(char), reinterpret_cast<int (*)(const void*, const void*)>(char_cmp)));
+    result = reinterpret_cast<char*>(bsearch(&key[0], &arr[0], n, sizeof(char), reinterpret_cast<int (*)(const void*, const void*)>(char_cmp)));
 
-	if(result) {
-		cout	<<result	<<"が一致します。\n";
+	if(result != NULL) {
+		cout	<<*result	<<"が一致します。\n";
+
 	} else {
 		cout	<<"見つかりませんでした。\n";
 	}
