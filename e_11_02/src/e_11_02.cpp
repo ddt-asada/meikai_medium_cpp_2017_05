ef4daf907baacd1e7b496eb848fce27cbab4ec61
ddt-asada <ddt.asada@gmail.com>
2017-06-09 13:31:18 +0900

    C++ 演習11-2 修正100％ （浅田 完了。

--- e_11_02/src/e_11_02.cpp
+++ e_11_02/src/e_11_02.cpp
@@ -2,6 +2,8 @@
  * 関数テンプレートでの文字列の走査を反復子を用いて実行できるように書き換えたプログラム。
  * 作成日：2017年6月6日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <string>
@@ -11,8 +13,8 @@
 using namespace std;
 
 //文字列ベクトルを走査する関数テンプレートの宣言。
-template<typename Allocator>
-void put_string_vector(const vector<string, Allocator>& v);
+template<class InputIterator>
+void put_string_vector(InputIterator first, InputIterator last);
 
 int main()
 {
@@ -29,25 +31,25 @@ int main()
 
 	//反復子をもちいて文字列を走査し、表示。
 	cout	<<"s1 = ";
-	put_string_vector(s1);
+	put_string_vector(s1.begin(), s1.end());
 	cout	<<'\n';
 
 	//反復子を用いて文字列を走査し、表示。
 	cout	<<"s2 = ";
-	put_string_vector(s2);
+	put_string_vector(s2.begin(), s2.end());
 	cout	<<'\n';
 
 	return 0;
 }
 
 //文字列ベクトルを走査する関数テンプレートの定義。
-template<typename Allocator>
-void put_string_vector(const vector<string, Allocator>& v)
+template<class InputIterator>
+void put_string_vector(InputIterator first, InputIterator last)
 {
 	cout	<<"{ ";
 
 	//文字列を表示する。
-	for(vector<string>::const_iterator i = v.begin(); i < v.end(); i++) {
+	for(InputIterator i = first; i < last; i++) {
 		cout	<<'"';
 		cout	<<*i;
 		cout	<<"\" ";
