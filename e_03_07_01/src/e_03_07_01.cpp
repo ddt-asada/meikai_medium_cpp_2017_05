3f8c0feff145a8766cf4f818f1c97256efee8e7d
ddt-asada <ddt.asada@gmail.com>
2017-06-09 12:56:35 +0900

    C++ 演習3-7 修正100％ （浅田 完了。

--- e_03_07_01/src/e_03_07_01.cpp
+++ e_03_07_01/src/e_03_07_01.cpp
@@ -2,6 +2,8 @@
  * qsort関数を用いて、配列を昇順にソートするプログラム。プログラム。
  * 作成日：2017年5月25日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <cstdlib>
@@ -16,21 +18,23 @@ int char_cmp(const char** a, const char** b);
 int main()
 {
 	//ソートするポインタによる文字列の宣言。
-	const char *arr[] = {"LISP", "C", "Ada", "Pascal"};
+	const char *arr[] = {"xyz", "adw", "afw", "afw", "awfa", "hrg","bfsr", "bmnn", "gesb", "gs", "gskdnersjkngnzbzd", "LISP", "C", "Ada", "Pascal","abc", "def", "ghi", "jkl", "lmn", "opq", "stu", "xyz", "adw", "afw", "afw", "awfa", "hrg","bfsr", "bmnn", "gesb", "gs", "gskdnersjkngnzbzd"};
+
+	const int n = sizeof(arr) / sizeof(arr[0]);		//要素数。
 
 	//ソートする前の文字列の表示。
 	cout	<<"以下の文字列を昇順にソートします。\n";
 
-	for(int i = 0; arr[i]; i++) {
+	for(int i = 0; i < n; i++) {
 		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
 	}
 
-	qsort(arr, sizeof(arr)/sizeof(arr[0]), sizeof(char *),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
+	qsort(&arr[0], n, sizeof(char *),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
 
 	//ソートし終わった文字列を表示。
 	cout	<<"\n文字列の並び替えが完了しました。\n結果は以下の通りです。\n";
 
-	for(int i = 0; arr[i]; i++) {
+	for(int i = 0; i < n; i++) {
 		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
 	}
 
--- e_03_07_02/src/e_03_07_02.cpp
+++ e_03_07_02/src/e_03_07_02.cpp
@@ -2,6 +2,8 @@
  * qsort関数を用いて、配列を昇順にソートするプログラム。
  * 作成日：2017年5月25日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <cstdlib>
@@ -15,22 +17,24 @@ int char_cmp(const char* a, const char* b);
 
 int main()
 {
+	const int n = 39;		//文字列の要素数。
+
 	//ソートするポインタによる文字列の宣言。
-	char arr[][7] = {"LISP", "C", "Ada", "Pascal"};
+	char arr[][n] = {"def", "ghi", "jkl", "lmn", "opq", "stu", "xyz", "adw", "afw", "afw", "awfa", "hrg","bfsr", "bmnn", "gesb", "gs", "gskdnersjkngnzbzd", "LISP", "C", "Ada", "Pascal","abc", "def", "ghi", "jkl", "lmn", "opq", "stu", "xyz", "adw", "afw", "afw", "awfa", "hrg","bfsr", "bmnn", "gesb", "gs", "gskdnersjkngnzbzd"};
 
 	//ソートする前の文字列の表示。
 	cout	<<"以下の文字列を昇順にソートします。\n";
 
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < n; i++) {
 		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
 	}
 
-	qsort(arr[0], sizeof(arr)/sizeof(arr[0]),sizeof(arr[0]),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
+	qsort(&arr, sizeof(arr)/sizeof(arr[0]),sizeof(arr[0]),reinterpret_cast<int (*)(const void*, const void*)>(char_cmp));
 
 	//ソートし終わった文字列を表示。
 	cout	<<"\n文字列の並び替えが完了しました。\n結果は以下の通りです。\n";
 
-	for(int i = 0; i < 4; i++) {
+	for(int i = 0; i < n; i++) {
 		cout	<<"arr["	<<i	<<"] = "	<<arr[i]	<<'\n';
 	}
 
