fe507cc83d321fca01921186a15442778d936ce5
ddt-asada <ddt.asada@gmail.com>
2017-06-09 13:28:59 +0900

    C++ 演習9-3 修正100％ （浅田 完了。

--- e_09_03/src/e_09_03.cpp
+++ e_09_03/src/e_09_03.cpp
@@ -2,6 +2,8 @@
  * Twin<Date>を要素として持つArray<>クラステンプレートオブジェクトを生成。
  * 作成日：2017年6月1日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <new>
@@ -15,7 +17,7 @@ using namespace std;
 int main()
 {
 	//配列クラステンプレートからDate型のオブジェクトを持つ配列を生成。
-	Array<Twin<Date> > x(3, (Twin<Date>(Date(1993, 9, 11), Date())));
+	Array<Twin<Twin<Date> > > x(3, (Twin<Twin<Date> >(Twin<Date>(Date(1993, 9, 11), Date()), Twin<Date>(Date(2000, 1, 22), Date(1223, 10, 20)))));
 
 	for(int i = 0; i < x.size(); i++) {
 		cout	<<"x["	<<i	<<"] = "	<<x[i]	<<'\n';
