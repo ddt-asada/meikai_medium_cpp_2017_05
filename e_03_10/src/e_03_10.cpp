ad06a2015e4dc4b6249be139a7c9ea43922c68e1
ddt-asada <ddt.asada@gmail.com>
2017-06-09 12:57:07 +0900

    C++ 演習3-10 修正100％ （浅田 完了。

--- e_03_10/src/e_03_10.cpp
+++ e_03_10/src/e_03_10.cpp
@@ -2,6 +2,8 @@
  * binsearchx関数を用いて文字列の配列からの探索を行うプログラム。
  * 作成日：2017年5月25日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <cstdlib>
@@ -14,20 +16,33 @@ using namespace std;
 void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*));
 
 //文字列の比較関数の宣言。
-int char_cmp(const char* a, const char*b);
+int int_cmp(const int* a, const int*b);
 
 int main()
 {
-	const char *arr[] = {"abc", "def", "ghi", "jkl", "lmn", "opq", "stu", "xyz"};
+	const int n = 50;		//配列の要素数。
 
-	char **result;
+	int* x = new int[n];	//配列を生成。
 
-	const char *key = "stasd";
+	int no = 21;			//探索する値。
 
-	result = reinterpret_cast<char**>(binsearchx(&key, arr, sizeof(arr)/sizeof(arr[0]), sizeof(char *), reinterpret_cast<int (*)(const void*, const void*)>(char_cmp)));
+	int* p;					//探索結果を指すポインタを生成。
 
-	if(result) {
-		cout	<<*result	<<"が一致します。\n";
+	//配列の各要素に値を代入する。
+	for(int i = 0; i < n; i += 3) {
+		x[i] = i;
+		cout	<<"x ["	<<i	<<"] = "	<<x[i]	<<'\n';
+		x[i + 1] = i;
+		cout	<<"x ["	<<i + 1	<<"] = "	<<x[i + 1]	<<'\n';
+		x[i + 2] = i;
+		cout	<<"x ["	<<i + 2	<<"] = "	<<x[i + 2]	<<'\n';
+
+	}
+
+	p = reinterpret_cast<int*>(binsearchx(&no, x, n, sizeof(int), reinterpret_cast<int (*)(const void*, const void*)>(int_cmp)));
+
+	if(p != NULL) {
+		cout	<<"x["	<<(p - x)	<<"]が一致します。\n";
 	} else {
 		cout	<<"見つかりませんでした。\n";
 	}
@@ -38,6 +53,8 @@ int main()
 //線形探索を行う関数の定義。
 void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*))
 {
+	void* place = 0;
+
 	if(nmemb > 0){
 		const char* x =reinterpret_cast<const char*>(base);
 		size_t pl = 0;			//探索範囲先頭の添字。
@@ -48,19 +65,21 @@ void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, i
 			int comp = compar(key, reinterpret_cast<const void*>(&x[(pc = (pl + pr) / 2) * size]));
 
 			//探索が成功した時。
-			if(comp == 0) {
-				for( ; pc > pl; pc--) {
-					if(compar(key, reinterpret_cast<const void*>(&x[(pc = (pl + pr) / 2) * size]))) {
+			if(comp < 0) {
+				for(;pc > 0; pc--) {
+					if(compar(key, reinterpret_cast<const void*>(&x[pc * size]))) {
+						pc += 1;
 						break;
 					}
 				}
 
-				return const_cast<void*>(reinterpret_cast<const void*>(&x[pc * size]));
-			//探索範囲がなくなったとき。
+				place = const_cast<void*>(reinterpret_cast<const void*>(&x[pc * size]));
+				break;
+				//探索範囲がなくなったとき。
 			} else if (pl == pr) {
 				break;
 			//探索範囲を後半に絞り込む。
-			} else if (comp > 0) {
+			} else if (comp < 0) {
 				pl = pc + 1;
 			//探索範囲を前半に絞り込む。
 			} else {
@@ -69,11 +88,20 @@ void* binsearchx(const void* key, const void* base, size_t nmemb, size_t size, i
 		}
 	}
 
-	return NULL;
+	return place;
 }
 
 //文字列の比較関数の定義。
-int char_cmp(const char* a, const char* b)
+int int_cmp(const int* a, const int* b)
 {
-	return strcmp(a, b);
+	int judge = 0;	//大小の判定を格納する変数。
+	//大小を比較しそれに応じた値を返す。
+		if(*a < *b) {
+			judge = 1;
+		} else if (*a > *b) {
+			judge = -1;
+		}
+
+		//判定を返す。
+		return judge;
 }
