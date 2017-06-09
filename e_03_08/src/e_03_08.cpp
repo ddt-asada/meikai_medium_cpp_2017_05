78aed9a1269fc63331f7bf477c0228128885811c
ddt-asada <ddt.asada@gmail.com>
2017-06-09 12:56:47 +0900

    C++ 演習3-8 修正100％ （浅田 完了。

--- e_03_08/src/e_03_08.cpp
+++ e_03_08/src/e_03_08.cpp
@@ -2,6 +2,8 @@
  * seqsearch関数を用いて文字列の配列からの探索を行うプログラム。
  * 作成日：2017年5月25日
  * 作成者：浅田　知嗣
+ * 更新日：2017年6月9日
+ * 更新者：浅田　知嗣
  */
 
 #include <cstdlib>
@@ -14,20 +16,33 @@ using namespace std;
 void* seqsearch(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*));
 
 //文字列の比較関数の宣言。
-int char_cmp(const char* a, const char*b);
+int int_cmp(const int* a, const int* b);
 
 int main()
 {
-	const char *arr[] = {"abc", "def", "ghi", "jkl", "lmn", "opq", "stu", "xyz"};
+	const int n = 100;			//配列の要素数。
 
-	char **result;
+	int* x = new int[n];		//配列を生成。
 
-	const char *key = "stu";
+	int no = 72;				//探索する値。
 
-	result = reinterpret_cast<char**>(seqsearch(&key, arr, sizeof(arr)/sizeof(arr[0]), sizeof(char *), reinterpret_cast<int (*)(const void*, const void*)>(char_cmp)));
+	int* p;						//探索結果を指すポインタを生成。
 
-	if(result) {
-		cout	<<*result	<<"が一致します。\n";
+	//配列の各要素に値を代入する。
+	//代入する値は3個連続して並べる。
+	for(int i = 0; i < n; i+= 3) {
+		x[i] = i;
+		cout	<<"x ["	<<i	<<"] = "	<<x[i]	<<'\n';
+		x[i + 1] = i;
+		cout	<<"x ["	<<i + 1	<<"] = "	<<x[i + 1]	<<'\n';
+		x[i + 2] = i;
+		cout	<<"x ["	<<i + 2	<<"] = "	<<x[i + 2]	<<'\n';
+	}
+
+	p = reinterpret_cast<int*>(seqsearch(&no, x, n, sizeof(int), reinterpret_cast<int (*)(const void*, const void*)>(int_cmp)));
+
+	if(p != NULL) {
+		cout	<<"x["	<<(p - x)	<<"]が一致します。\n";
 	} else {
 		cout	<<"見つかりませんでした。\n";
 	}
@@ -38,18 +53,31 @@ int main()
 //線形探索を行う関数の定義。
 void* seqsearch(const void* key, const void* base, size_t nmemb, size_t size, int (*compar)(const void*, const void*))
 {
+	void* place = NULL;		//みつけた場所を返す変数。
+
 	const char* x =reinterpret_cast<const char*>(base);
 
 	for(size_t i = 0;i < nmemb; i++) {
 		if(!compar(key, reinterpret_cast<const void*>(&x[i* size]))) {
-			return const_cast<void*>(reinterpret_cast<const void*>(&x[i *size]));
+			place = const_cast<void*>(reinterpret_cast<const void*>(&x[i *size]));
+			break;
 		}
 	}
-	return NULL;
+	return place;
 }
 
-//文字列の比較関数の定義。
-int char_cmp(const char* a, const char* b)
+//数値の比較関数の定義。
+int int_cmp(const int* a, const int* b)
 {
-	return strcmp(a, b);
+	int judge = 0;		//判定
+
+	//大小を比較しそれに応じた値を返す。
+	if(*a < *b) {
+		judge = 1;
+	} else if (*a > *b) {
+		judge = -1;
+	}
+
+	//判定を返す。
+	return judge;
 }
